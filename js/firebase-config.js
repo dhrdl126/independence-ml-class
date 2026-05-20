63b04bd968289196dddd00423cb6fa022e9d36d3
dhrdl126 <dhrdl126@gmail.com>
2026-05-21 00:15:49 +0900

    Set Firebase web config

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -2,14 +2,13 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebas
 import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore.js";
 
-// TODO: Firebase 콘솔에서 복사한 값으로 교체
 const firebaseConfig = {
-  apiKey: "TODO",
-  authDomain: "TODO",
-  projectId: "TODO",
-  storageBucket: "TODO",
-  messagingSenderId: "TODO",
-  appId: "TODO"
+  apiKey: "AIzaSyCBQ2xJQ9Wh_kVk47FB2maENF_ztiIzWhU",
+  authDomain: "independence-ml-class.firebaseapp.com",
+  projectId: "independence-ml-class",
+  storageBucket: "independence-ml-class.firebasestorage.app",
+  messagingSenderId: "905137308621",
+  appId: "1:905137308621:web:58b8c16f2e27bac9023a84"
 };
 
 export const app = initializeApp(firebaseConfig);
